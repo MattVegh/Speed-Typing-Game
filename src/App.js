debfaf2aeda854b032391f40ff57aa48a22ad712
Matt Vegh <mdv240@gmail.com>
2020-03-24 14:29:59 -0500

    frame

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>yo</h1>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>time remaing placeholder</h4>
+      <button>Start</button>
+      <h1>Word Count: p14c3h01d3r</h1>
     </div>
   );
 }
