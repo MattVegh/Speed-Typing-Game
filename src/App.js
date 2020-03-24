0e82012b72a949e303b63d2f973dda8cf60997ba
Matt Vegh <mdv240@gmail.com>
2020-03-24 14:48:59 -0500

    setting state for textbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [words, setWords] = useState('')
+
+  function getTypedWords(e) {
+    const { value } = e.target
+    setWords(value)
+  }
+
+  console.log(words)
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>time remaing placeholder</h4>
+      <textarea
+        onChange={getTypedWords}
+        value={words}
+      />
+      <h4>time remaining placeholder</h4>
       <button>Start</button>
       <h1>Word Count: p14c3h01d3r</h1>
     </div>
