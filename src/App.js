f13b1f60ad6c21d0517e77d526c7693c16a8c03a
Matt Vegh <mdv240@gmail.com>
2020-03-28 18:34:06 -0500

    refactored for custom hooks

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import './App.css';
+import useWordGame from './hooks/useWordGame'
+
 
 function App() {
-  const STARTING_TIME = 5
 
-  const [words, setWords] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-
-  const textBoxRef = useRef(null)
-
-  function getTypedWords(e) {
-    const { value } = e.target
-    setWords(value)
-  }
-
-  function calculateWordCount(text) {
-    const wordsArray = text.trim().split(' ')
-    const filteredArray = wordsArray.filter(word => word !== '').length
-
-    return filteredArray
-  }
-
-  function startGame() {
-    setIsTimeRunning(true)
-    setTimeRemaining(STARTING_TIME)
-    setWords('')
-    console.log(textBoxRef)
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-
-  function endGame() {
-    setIsTimeRunning(false)
-    setWordCount(calculateWordCount(words))
-  }
-  useEffect(() => {
-
-    if (isTimeRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining(time => time - 1)
-      }, 1000)
-    }
-
-    // can also just do "else"
-    if (timeRemaining === 0) {
-      endGame()
-    }
-  }, [timeRemaining, isTimeRunning])
+  const { textBoxRef, getTypedWords, words, timeRemaining, startGame, isTimeRunning, wordCount } = useWordGame()
 
   return (
     <div className="App">
